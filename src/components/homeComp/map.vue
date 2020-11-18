2db99d512ef89f048dbc42179289ad5e7ee58b4f
djh <1786008022@qq.com>
2020-11-18 10:42:56 +0800

    首页

---
+++ src/components/homeComp/map.vue
@@ -0,0 +1,37 @@
+<template>
+     <div id="map" style="width:100%;height:6rem;"></div>
+</template>
+
+<script> 
+//所有的独立组件均存放在 vue-baidu-map/components 文件夹下，按需引用即可
+import BaiduMap from "vue-baidu-map/components/map/Map.vue"; 
+import BmNavigation from "vue-baidu-map/components/controls/Navigation.vue";
+export default {
+  components: {
+    BaiduMap,
+    BmNavigation,
+  },
+  data() {
+    return {
+   
+    };
+  },
+  mounted() {
+            // 百度地图API功能
+            var map = new BMap.Map("map");    // 创建Map实例
+            map.centerAndZoom(new BMap.Point(113.687202,34.810331), 22);  // 初始化地图,设置中心点坐标和地图级别
+            //添加地图类型控件
+            map.addControl(new BMap.MapTypeControl({
+                mapTypes:[
+                    BMAP_NORMAL_MAP,
+                    BMAP_HYBRID_MAP
+                ]}));
+            map.setCurrentCity("郑州");          // 设置地图显示的城市 此项是必须设置的
+            map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+  },
+};
+</script>
+
+<style lang="less" scoped>
+  
+</style>
---
+++ src/components/homeComp/userinfo.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="userinfo">
+    <div class="user">
+      <div><img :src="img" alt="" /></div>
+      <div>
+        <p>{{ name }}</p>
+        <p>{{ phone }}</p>
+      </div>
+      <div>
+        <!-- 路由路径暂时未配置 -->
+        <router-link to="#">
+          <van-icon
+            name="arrow"
+            class="right_arrow"
+            size="30"
+            color="#ffffff"
+          />
+        </router-link>
+        <router-view></router-view>
+      </div>
+    </div>
+    <div>
+      <ul class="lis">
+        <li>
+          <router-link to="#">
+            <van-icon name="smile" size="40" color="#42a3fe" />
+            <p>我的钱包</p>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="#">
+            <van-icon name="smile" size="40" color="#42a3fe" />
+            <p>我的订单</p>
+          </router-link>
+        </li>
+
+        <li>
+          <router-link to="#">
+            <van-icon name="smile" size="40" color="#42a3fe" />
+            <p>用户反馈</p>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="#">
+            <van-icon name="smile" size="40" color="#42a3fe" />
+            <p>帮助中心</p>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="#">
+            <van-icon name="smile" size="40" color="#42a3fe" />
+            <p>设置</p>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="#">
+            <van-icon name="smile" size="40" color="#42a3fe" />
+            <p>版本检测</p>
+          </router-link>
+        </li>
+      </ul>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      img: require("./img/user.jpg"),
+      name: "李先生",
+      phone: "176****6785",
+    };
+  },
+  created() {},
+  watch: {},
+  computed: {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang='less' scoped>
+html,
+body {
+  height: 100%;
+}
+
+.userinfo {
+  width: 5rem;
+  height: 100%;
+  background-color: #ffff;
+  .user {
+    padding-top: 0.8rem;
+    height: 3.07rem;
+    align-items: center;
+    background-color: dodgerblue;
+    border-radius: 0 0.45rem 0 0;
+    display: flex;
+    div:nth-of-type(1) {
+      margin-left: 0.23rem;
+      img {
+        width: 1.3rem;
+        height: 1.3rem;
+        border-radius: 50%;
+      }
+    }
+    div:nth-of-type(2) {
+      margin-left: 0.28rem;
+      p {
+        color: white;
+      }
+      p:nth-of-type(1) {
+        font-size: 0.28rem;
+      }
+      p:nth-of-type(2) {
+        font-size: 0.24rem;
+      }
+    }
+    div:nth-of-type(3) {
+      margin-left: 1.2rem;
+    }
+  }
+  .lis{
+      li{
+          height: 1rem;
+          line-height: 1rem;
+          margin-left: .25rem;
+          a{
+              display: flex;
+              p{
+                  font-size: .28rem;
+                  margin-left: .35rem;
+                  margin-top: .05rem;
+                  font-weight: 600;
+              }
+              i{
+                  display: inline-block;
+                  margin-top: .2rem;
+                  
+              }
+          }
+      }
+  }
+}
+</style>
